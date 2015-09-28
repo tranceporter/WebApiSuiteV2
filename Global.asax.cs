b6e22a0c2de45ded7c2fb2d2fb329565049a607d
shreyas <shreyas.zanpure@gmail.com>
2015-09-28 20:05:19 +0100

    Added code for masking

--- Global.asax.cs
+++ Global.asax.cs
@@ -1,7 +1,10 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Net;
+using System.Threading;
 using System.Web;
+using System.Web.Caching;
 using System.Web.Http;
 using System.Web.Mvc;
 using System.Web.Optimization;
@@ -22,6 +25,54 @@ namespace WebAPISuite
             FilterConfig.RegisterGlobalFilters(GlobalFilters.Filters);
             RouteConfig.RegisterRoutes(RouteTable.Routes);
             BundleConfig.RegisterBundles(BundleTable.Bundles);
+
+            SetupRefreshJob();
+        }
+
+        private static void SetupRefreshJob()
+        {
+            //remove a previous job
+            Action remove = HttpContext.Current.Cache["WebApiSuiteRefresh"] as Action;
+            if (remove is Action)
+            {
+                HttpContext.Current.Cache.Remove("WebApiSuiteRefresh");
+                remove.EndInvoke(null);
+            }
+
+            //get the worker
+            Action work = () =>
+            {
+                while (true)
+                {
+                    Thread.Sleep(60000);
+                    WebClient refresh = new WebClient();
+                    try
+                    {
+                        refresh.UploadString("http://www.elementalstudios.co.uk/Home/Contact", string.Empty);
+                        refresh.DownloadString("http://elementalstudios.co.uk/api/SendEmailApi/GetClientSettings?clientName=Shreyas");
+                    }
+                    catch (Exception)
+                    {
+                        //snip...
+                    }
+                    finally
+                    {
+                        refresh.Dispose();
+                    }
+                }
+            };
+            work.BeginInvoke(null, null);
+
+            //add this job to the cache
+            HttpContext.Current.Cache.Add(
+                "WebApiSuiteRefresh",
+                work,
+                null,
+                Cache.NoAbsoluteExpiration,
+                Cache.NoSlidingExpiration,
+                CacheItemPriority.Normal,
+                (s, o, r) => { SetupRefreshJob(); }
+                );
         }
     }
 }
